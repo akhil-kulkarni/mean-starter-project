080a9badd1040f2f3bac75a0254cc61a406ad9e5
Akhil Kulkarni <akhil16.4.93@gmail.com>
2016-07-06 23:18:17 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,4 @@
+// config/db.js
+	module.exports = {
+		url : 'mongodb://localhost/mean-starter-db' //127.0.0.1:50852
+	}
---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,4 @@
+// public/js/controllers/NerdCtrl.js
+angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+	$scope.tagline = 'Nothing beats a pocket protector!';
+});
